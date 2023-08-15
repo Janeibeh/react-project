942c36be36b09f5b5781cf3d2391607e12718b38
Janeibeh <janewhite.ji@gmail.com>
2023-08-15 10:01:01 +0100

    Portfolio setup

---
+++ Desktop/react-project/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css"; 
+import './App.css';
+
+
+function App() {
+  return (
+    <h2>
+      portfolio project...
+    </h2>
+  );
+}
+
+export default App;
